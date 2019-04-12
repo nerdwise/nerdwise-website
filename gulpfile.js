739ee0b2bd1b254135b6d0c4ccfa8933485612b7
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-04-12 14:15:33 -0700

    Fix css re-compilation problems with gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-var extend = require("deep-extend");
-var gulp = require("gulp");
-var gulpAutoprefixer = require("gulp-autoprefixer");
-var path = require("path");
-var rename = require("gulp-rename");
-var sass = require("gulp-sass");
-var webpack = require("webpack");
-var webpackStream = require("webpack-stream");
-var exec = require('gulp-exec');
+const extend = require("deep-extend");
+const gulp = require("gulp");
+const gulpAutoprefixer = require("gulp-autoprefixer");
+const path = require("path");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const webpack = require("webpack");
+const webpackStream = require("webpack-stream");
+const exec = require("gulp-exec");
 
-var config = {
+const config = {
   TS_SOURCES: ["./global/*.ts", "./components/**/*.ts"],
   JS_SOURCE_FILE: "./tmp/js/global/main.js",
   JS_SOURCES: ["./tmp/js/**/*.js"],
@@ -23,7 +23,7 @@ var config = {
   SASS_OUT_DIR: "./dist/css/"
 };
 
-var webpackConfig = {
+const webpackConfig = {
   entry: { main: config.JS_SOURCE_FILE },
   mode: "development",
   output: {
@@ -32,7 +32,7 @@ var webpackConfig = {
   }
 };
 
-var webpackProdConfig = extend(
+const webpackProdConfig = extend(
   {
     mode: "production"
   },
@@ -41,13 +41,13 @@ var webpackProdConfig = extend(
 
 const compileJs = () => {
   return gulp
-      .src(config.JS_SOURCES)
-      .pipe(webpackStream(webpackProdConfig, webpack))
-      .pipe(gulp.dest(config.JS_OUT_DIR));
+    .src(config.JS_SOURCES)
+    .pipe(webpackStream(webpackProdConfig, webpack))
+    .pipe(gulp.dest(config.JS_OUT_DIR));
 };
 gulp.task("compile-js", compileJs);
 
-const compileSass = () => {
+const buildNewSass = () => {
   return gulp
     .src(config.SASS_SOURCE_FILE)
     .pipe(
@@ -68,6 +68,14 @@ const compileSass = () => {
     )
     .pipe(gulp.dest(config.SASS_OUT_DIR));
 };
+
+const touchSass = () => {
+  return gulp
+    .src("./dist/css/main.min.css", { allowEmpty: true })
+    .pipe(exec("touch <%= file.path %>"));
+};
+
+const compileSass = gulp.series(buildNewSass, touchSass);
 gulp.task("compile-sass", compileSass);
 
 const watchSass = () => gulp.watch(config.SASS_SOURCES, compileSass);
@@ -76,20 +84,20 @@ gulp.task("watch-sass", watchSass);
 const watchTs = () => gulp.watch(config.TS_SOURCES, compileTs);
 gulp.task("watch-ts", watchTs);
 
-const clearOldTs = () =>{
-  return gulp.src('./tmp/js', {allowEmpty: true})
-      .pipe(exec('rm -rf <%= file.path %>'));
+const clearOldTs = () => {
+  return gulp
+    .src("./tmp/js", { allowEmpty: true })
+    .pipe(exec("rm -rf <%= file.path %>"));
 };
 gulp.task("clear-old-ts", clearOldTs);
 
 const buildNewTs = () => {
-  return gulp.src('./tsconfig.json')
-      .pipe(exec('tsc -p <%= file.path %>'));
+  return gulp.src("./tsconfig.json").pipe(exec("tsc -p <%= file.path %>"));
 };
 gulp.task("build-new-ts", buildNewTs);
 
-const sleep = (done) => {
-  exec('sleep 10');
+const sleep = done => {
+  exec("sleep 10");
   done();
 };
 const compileTs = gulp.series(clearOldTs, buildNewTs, sleep, compileJs);
