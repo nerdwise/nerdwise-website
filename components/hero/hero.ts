25b8f55a0d48d6dfa9c91040dafc1896326a8cd8
Angus MacDonald <angusamacdonald@hotmail.com>
2019-04-26 01:34:43 -0600

    Subtle rotate on hero

--- components/hero/hero.ts
+++ components/hero/hero.ts
@@ -16,14 +16,14 @@ class Hero {
     this.scrollEffect_ = new ScrollEffect(this.header_, {
       effects: [
         new Tween([
-          [0, 'opacity: 1; transform: scale(1)'],
-          [1, 'opacity: 0; transform: scale(0.9)']
+          [0, 'opacity: 1; transform: scale(1) rotate(0deg) translateY(0)'],
+          [1, 'opacity: 0; transform: scale(0.9) rotateX(3deg) translateY(-5vh)']
         ]),
         new Tween([[0, 'opacity: 0'], [1, 'opacity: 1']], {
           styleTarget: this.overlay_
         })
       ],
-      getDistanceFunction: DistanceFunction.DISTANCE_FROM_DOCUMENT_CENTER,
+      getDistanceFunction: DistanceFunction.DOCUMENT_SCROLL,
       startDistance: () => 0,
       endDistance: window.innerHeight / 3
     });
