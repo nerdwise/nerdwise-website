3fdbf2c17c131c470bfe171921ad4f3b002a1ef5
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-05-03 13:08:40 -0700

    Actually remove comments

--- components/nav/nav.ts
+++ components/nav/nav.ts
@@ -4,26 +4,11 @@ import { renderLoop } from '../../node_modules/toolbox-v2/src/toolbox/utils/rend
 import { getVisibleDistanceFromRoot } from '../../node_modules/toolbox-v2/src/toolbox/utils/dom/position/vertical/get-visible-distance-from-root';
 import { max } from '../../node_modules/toolbox-v2/src/toolbox/utils/array/max';
 
-// const SECTIONS = ['workflow', 'projects', 'about', 'contact'];
-
 class Nav {
-  // private readonly about_: HTMLElement;
-  // private readonly projects_: HTMLElement;
-  // private readonly workflow_: HTMLElement;
-  // private readonly contact_: HTMLElement;
-  // private sectionWatcher_: ActiveOnCondition = null;
-  // private hero_: HTMLElement;
-  // private navBackground_: HTMLElement;
   private sections_: HTMLElement[];
   private navLinks_: HTMLElement[];
 
   constructor() {
-    // this.hero_ = document.querySelector('.hero');
-    // this.navBackground_ = document.querySelector('.nav__background');
-    // this.about_ = document.querySelector('#about');
-    // this.projects_ = document.querySelector('#projects');
-    // this.workflow_ = document.querySelector('#workflow');
-    // this.contact_ = document.querySelector('#contact');
     this.sections_ = Array.from(document.querySelectorAll('.section'));
     this.navLinks_ = Array.from(document.querySelectorAll('.nav__link'));
   }
@@ -44,38 +29,6 @@ class Nav {
       });
     });
   }
-
-  // getElementYPosition(sectionIndex: number): number {
-  //   const SECTION_ELEMENTS = [
-  //     this.workflow_,
-  //     this.projects_,
-  //     this.about_,
-  //     this.contact_
-  //   ];
-
-  //   if (typeof SECTION_ELEMENTS[sectionIndex] === 'undefined') {
-  //     return 999999;
-  //   } else {
-  //     return SECTION_ELEMENTS[sectionIndex].offsetTop - 30;
-  //   }
-  // }
-
-  // currentSection(): void {
-  //   SECTIONS.map((section, sectionIndex) => {
-  //     this.sectionWatcher_ = new ActiveOnCondition(
-  //       `nav__link--${section}`,
-  //       () => {
-  //         return (
-  //           Scroll.getSingleton().getPosition().y >
-  //             this.getElementYPosition(sectionIndex) &&
-  //           Scroll.getSingleton().getPosition().y <
-  //             this.getElementYPosition(sectionIndex + 1)
-  //         );
-  //       },
-  //       'active'
-  //     );
-  //   });
-  // }
 
   init(): void {
     this.currentSection();
