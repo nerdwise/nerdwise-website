2b6c987b5f8177a969dc65e8fad6a0b567bf0c6f
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-05-14 14:14:56 -0700

    Begin lazy loader setup

---
+++ components/projects/projects.ts
@@ -0,0 +1,26 @@
+import { loadImage } from '../../node_modules/toolbox-v2/src/toolbox/utils/loading/load-image';
+
+class Projects {
+  private projectImages_: HTMLImageElement[];
+
+  constuctor() {
+    this.projectImages_ = Array.from(
+      document.querySelectorAll('.project__image')
+    );
+  }
+
+  init(): void {
+    this.lazyLoad();
+  }
+
+  lazyLoad(): void {
+    this.projectImages_.forEach(image => {
+      const imageUrl: string = image.dataset.src;
+      loadImage(imageUrl).then(() => {
+        image.src = imageUrl;
+      });
+    });
+  }
+}
+
+export { Projects };
