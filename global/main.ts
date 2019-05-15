71f036ccc5a2a76603892dcda36ca2871c9c975b
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-05-15 14:12:44 -0700

    Build error

--- global/main.ts
+++ global/main.ts
@@ -6,26 +6,29 @@ import { SkewedSection } from '../components/skewed-section/skewed-section';
 import { WhyUs } from '../components/why-us/why-us';
 import { initNav } from './ts-inits/init-nav';
 import { Projects } from '../components/projects/projects';
+import { onDomContentLoad } from '../../toolbox-v2/src/toolbox/utils/dom/on-dom-content-load';
 
-const blocks = new Blocks();
-blocks.init();
+onDomContentLoad(() => {
+  const blocks = new Blocks();
+  blocks.init();
 
-initNav();
+  initNav();
 
-const about = new About();
-about.init();
+  const about = new About();
+  about.init();
 
-const hero = new Hero();
-hero.init();
+  const hero = new Hero();
+  hero.init();
 
-const contact = new Contact();
-contact.init();
+  const contact = new Contact();
+  contact.init();
 
-const skewedSection = new SkewedSection();
-skewedSection.startScrollEffect();
+  const skewedSection = new SkewedSection();
+  skewedSection.startScrollEffect();
 
-const whyUs = new WhyUs();
-whyUs.init();
+  const whyUs = new WhyUs();
+  whyUs.init();
 
-const projects = new Projects();
-projects.init();
+  const projects = new Projects();
+  projects.init();
+});
