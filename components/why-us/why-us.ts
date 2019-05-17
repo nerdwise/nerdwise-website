a4e15e9e53920fafc2d331ec4b4f254531b0181f
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-05-17 15:42:54 -0700

    Fix why-us modal

--- components/why-us/why-us.ts
+++ components/why-us/why-us.ts
@@ -1,15 +1,18 @@
 class WhyUs {
   private modal_: HTMLElement;
   private button_: HTMLElement;
+  private whyUsContent_: HTMLElement;
 
   constructor() {
     this.modal_ = document.querySelector('.modal');
     this.button_ = document.querySelector('.why-us__button');
+    this.whyUsContent_ = document.querySelector('.why-us__content');
   }
 
   openModal(): void {
     this.button_.addEventListener('click', () => {
       this.modal_.classList.add('display');
+      this.whyUsContent_.classList.add('hide');
     });
     this.closeModal();
   }
@@ -18,11 +21,13 @@ class WhyUs {
     window.addEventListener('click', event => {
       if (event.target == this.modal_) {
         this.modal_.classList.remove('display');
+        this.whyUsContent_.classList.remove('hide');
       }
     });
     const x: HTMLElement = document.querySelector('.modal__x');
     x.addEventListener('click', () => {
       this.modal_.classList.remove('display');
+      this.whyUsContent_.classList.remove('hide');
     });
   }
 
