d83fc934cdba843cf0ca696342705056e1cbbbc5
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-05-21 14:10:42 -0700

    Begin why us accordion setup

--- components/why-us/why-us.ts
+++ components/why-us/why-us.ts
@@ -1,12 +1,16 @@
+import { Accordion } from 'toolbox/components/accordion/base';
+
 class WhyUs {
-  private modal_: HTMLElement;
-  private button_: HTMLElement;
-  private whyUsContent_: HTMLElement;
+  private readonly modal_: HTMLElement;
+  private readonly button_: HTMLElement;
+  private readonly whyUsContent_: HTMLElement;
+  private readonly modalContent_: HTMLElement;
 
   constructor() {
     this.modal_ = document.querySelector('.modal');
     this.button_ = document.querySelector('.why-us__button');
     this.whyUsContent_ = document.querySelector('.why-us__content');
+    this.modalContent_ = document.querySelector('.modal__content');
   }
 
   openModal(): void {
@@ -31,8 +35,17 @@ class WhyUs {
     });
   }
 
+  accordion(): void {
+    new Accordion(this.modalContent_, {
+      contentSelector: 'modal__list',
+      itemSelector: 'modal__item',
+      toggleSelector: 'modal__question'
+    });
+  }
+
   init(): void {
     this.openModal();
+    this.accordion();
   }
 }
 
